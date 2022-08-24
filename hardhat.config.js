438738dc790f7831e6b9dca65ab4a940672c345e
Umair Mirza <umair_hp@hotmail.com>
2022-08-24 22:10:49 +0500

    added hardhat-deploy-ethers

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-deploy");
+require("hardhat-deploy-ethers");
 require("solidity-coverage");
 require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
