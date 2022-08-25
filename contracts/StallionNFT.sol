94a47f3f6e95e54bfcdf08243d55abcdd4b22748
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-08-25 07:40:24 +0100

    added tests

--- contracts/StallionNFT.sol
+++ contracts/StallionNFT.sol
@@ -16,22 +16,24 @@ contract StallionNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     // Enums
     enum Level {
         Bullet,
-        Riffle,
+        Hope,
         Flash
     }
 
     struct Attributes {
         uint256 mintprice;
         uint256 supply;
+        string name;
+        string metadata;
     }
 
     mapping(uint256 => Attributes) private _levels;
     mapping(address => uint256) private horseLevelOwned;
 
     constructor() ERC721("StallionRun", "SRN") {
-        _levels[0] = Attributes(0.001 ether, 0);
-        _levels[1] = Attributes(0.002 ether, 0);
-        _levels[2] = Attributes(0.003 ether, 0);
+        _levels[0] = Attributes(0.001 ether, 0, "Bullet", "1");
+        _levels[1] = Attributes(0.002 ether, 0, "Hope", "2");
+        _levels[2] = Attributes(0.003 ether, 0, "Flash", "3");
     }
 
     /**
@@ -39,7 +41,7 @@ contract StallionNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
      */
     function _baseURI() internal view virtual override returns (string memory) {
         return
-            "https://ipfs.io/ipfs/bafybeieic5yzvavzl4udjzcg4tnun3bqwp4ec2lgd6dayfobm5otez3nmu/";
+            "https://ipfs.io/ipfs/bafybeieic5yzvavzl4udjzcg4tnun3bqwp4ec2lgd6dayfobm5otez3nmu/metadata/";
     }
 
     function _beforeTokenTransfer(
@@ -84,6 +86,11 @@ contract StallionNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         return super.tokenURI(tokenId);
     }
 
+    /**
+     * @notice mints token to msg.sender
+     * @param _level level of horse to mint
+     * @dev only one mint per address, address has to burn token to mint another token
+     */
     function mint(Level _level) public payable {
         require(balanceOf(msg.sender) < 1, "only 1 NFT per address");
         require(_counter < _TOTAL_MAX_SUPPLY, "Total supply exceeded");
@@ -91,19 +98,22 @@ contract StallionNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         uint256 level = uint8(_level);
 
         require(
-            _levels[uint8(_level)].supply <= _LEVEL_MAX_SUPPLY,
+            _levels[level].supply <= _LEVEL_MAX_SUPPLY,
             "level max supply reached"
         );
         require(msg.value != _levels[level].mintprice, "mint price is not met");
 
-        uint256 tokenId = _counter++;
-        string memory _tokenURI = tokenURI(level);
-
+        uint256 tokenId = ++_counter;
         _mint(msg.sender, tokenId);
+
+        string memory _tokenURI = _levels[level].metadata;
         _setTokenURI(tokenId, _tokenURI);
-        _counter++;
         _levels[level].supply += 1;
         horseLevelOwned[msg.sender] = level;
+    }
+
+    function burn(uint256 tokenId) external {
+        _burn(tokenId);
     }
 
     function withdraw() external onlyOwner {
