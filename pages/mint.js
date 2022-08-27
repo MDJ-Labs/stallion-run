9e1ee4203d0b0f73ce92aab3cbd9ded551396507
John <jooohn.eth@gmail.com>
2022-08-27 14:53:55 -0400

    test commit

--- pages/mint.js
+++ pages/mint.js
@@ -1,83 +1,80 @@
-import {useWeb3Contract} from 'react-moralis'
-import styles from "../styles/Mint.module.css"
-import {abi, contractAddresses} from '../constants'
-import {useMoralis} from 'react-moralis'
-import { useEffect, useState } from 'react'
-import {ethers} from 'ethers'
+import styles from "../styles/Mint.module.css";
+import { abi, contractAddresses } from "../constants";
+import { useMoralis } from "react-moralis";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 const Mint = () => {
+  const [mintedBullets, setMintedBullets] = useState(0);
+  const [mintedHopes, setMintedHopes] = useState(0);
+  const [mintedFlashes, setMintedFlashes] = useState(0);
 
-    const [mintedBullets, setMintedBullets] = useState(0);
-    const [mintedHopes, setMintedHopes] = useState(0);
-    const [mintedFlashes, setMintedFlashes] = useState(0);
+  const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+  const chainId = parseInt(chainIdHex);
+  const stallionRunAddress =
+    chainId in contractAddresses ? contractAddresses[chainId][0] : null;
 
-    const {chainId: chainIdHex, isWeb3Enabled} = useMoralis()
-    const chainId = parseInt(chainIdHex)
-    const stallionRunAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null 
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      nftsMinted();
+    }
+  }, [isWeb3Enabled]);
 
+  const mintBullet = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(stallionRunAddress, abi, signer);
+      await contract.mint(0, {
+        value: ethers.utils.parseEther("0.001"),
+      });
+      nftsMinted();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-        if(isWeb3Enabled) {
-            nftsMinted();
-        }
-    }, [isWeb3Enabled])
-    
+  const mintHope = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(stallionRunAddress, abi, signer);
+      await contract.mint(1, {
+        value: ethers.utils.parseEther("0.002"),
+      });
+      nftsMinted();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const mintBullet = async () => {
-        try {
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner();
-          const contract = new ethers.Contract(stallionRunAddress, abi, signer);
-          await contract.mint(0, {
-            value: ethers.utils.parseEther("0.001"),
-          });
-          nftsMinted();
-        } catch (error) {
-          console.error(error);
-        }
-      };
-    
-      const mintHope = async () => {
-        try {
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner();
-          const contract = new ethers.Contract(stallionRunAddress, abi, signer);
-          await contract.mint(1, {
-            value: ethers.utils.parseEther("0.002"),
-          });
-          nftsMinted();
-        } catch (error) {
-          console.error(error);
-        }
-      };
-    
-      const mintFlash = async () => {
-        try {
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner();
-          const contract = new ethers.Contract(stallionRunAddress, abi, signer);
-          await contract.mint(2, {
-            value: ethers.utils.parseEther("0.003"),
-          });
-          nftsMinted();
-        } catch (error) {
-          console.error(error);
-        }
-      };
+  const mintFlash = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(stallionRunAddress, abi, signer);
+      await contract.mint(2, {
+        value: ethers.utils.parseEther("0.003"),
+      });
+      nftsMinted();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-      const nftsMinted = async () => {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const contract = new ethers.Contract(stallionRunAddress, abi, provider);
-        let bulletSupplyBI = await contract.bulletSupply();
-        let hopeSupplyBI = await contract.hopeSupply();
-        let flashSupplyBI = await contract.flashSupply();
-        let bulletSupply = parseInt(bulletSupplyBI);
-        let hopeSupply = parseInt(hopeSupplyBI);
-        let flashSupply = parseInt(flashSupplyBI);
-        setMintedBullets(bulletSupply);
-        setMintedHopes(hopeSupply);
-        setMintedFlashes(flashSupply);
-      };
+  const nftsMinted = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(stallionRunAddress, abi, provider);
+    let bulletSupplyBI = await contract.bulletSupply();
+    let hopeSupplyBI = await contract.hopeSupply();
+    let flashSupplyBI = await contract.flashSupply();
+    let bulletSupply = parseInt(bulletSupplyBI);
+    let hopeSupply = parseInt(hopeSupplyBI);
+    let flashSupply = parseInt(flashSupplyBI);
+    setMintedBullets(bulletSupply);
+    setMintedHopes(hopeSupply);
+    setMintedFlashes(flashSupply);
+  };
 
   return (
     <div className="">
@@ -90,62 +87,63 @@ const Mint = () => {
         </p>
       </div>
       {stallionRunAddress ? (
-            <div className="mx-auto max-w-7xl grid grid-cols-3 gap-8 py-24 px-4 sm:px-6 lg:px-8">
-            <div className={styles.card}>
-              <p className={styles.cardMinted}>{mintedBullets}/3000</p>
-              <h1 className={styles.cardTitle}>Bullet</h1>
-              <p className={styles.cardDesc}>
-                Bullet is a horse with average speed. Speed increment: 3.
+        <div className="mx-auto max-w-7xl grid grid-cols-3 gap-8 py-24 px-4 sm:px-6 lg:px-8">
+          <div className={styles.card}>
+            <p className={styles.cardMinted}>{mintedBullets}/3000</p>
+            <h1 className={styles.cardTitle}>Bullet</h1>
+            <p className={styles.cardDesc}>
+              Bullet is a horse with average speed. Speed increment: 3.
+            </p>
+            <div className={styles.cardPriceCont}>
+              <p className={styles.cardPrice}>
+                <span>{0.001}</span>
+                <span> ETH</span>
               </p>
-              <div className={styles.cardPriceCont}>
-                <p className={styles.cardPrice}>
-                  <span>{0.001}</span>
-                  <span> ETH</span>
-                </p>
-              </div>
-              <button className={styles.cardMint} onClick={() => mintBullet()}>
-                Mint Bullet
-              </button>
             </div>
-            <div className={styles.card}>
-              <p className={styles.cardMinted}>{mintedHopes}/3000</p>
-              <h1 className={styles.cardTitle}>Hope</h1>
-              <p className={styles.cardDesc}>
-                Hope is a horse with fast speed. Speed increment: 6.
-              </p>
-              <div className={styles.cardPriceCont}>
-                <p className={styles.cardPrice}>
-                  <span>{0.002}</span>
-                  <span> ETH</span>
-                </p>
-              </div>
-              <button className={styles.cardMint} onClick={() => mintHope()}>
-                Mint Hope
-              </button>
-            </div>
-            <div className={styles.card}>
-              <p className={styles.cardMinted}>{mintedFlashes}/3000</p>
-              <h1 className={styles.cardTitle}>Flash</h1>
-              <p className={styles.cardDesc}>
-                Bullet is a horse with insane speed. Speed Increment: 9.
-              </p>
-              <div className={styles.cardPriceCont}>
-                <p className={styles.cardPrice}>
-                  <span>{0.003}</span>
-                  <span> ETH</span>
-                </p>
-              </div>
-              <button className={styles.cardMint} onClick={() => mintFlash()}>
-                Mint Flash
-              </button>
-            </div>
+            <button className={styles.cardMint} onClick={() => mintBullet()}>
+              Mint Bullet
+            </button>
           </div>
+          <div className={styles.card}>
+            <p className={styles.cardMinted}>{mintedHopes}/3000</p>
+            <h1 className={styles.cardTitle}>Hope</h1>
+            <p className={styles.cardDesc}>
+              Hope is a horse with fast speed. Speed increment: 6.
+            </p>
+            <div className={styles.cardPriceCont}>
+              <p className={styles.cardPrice}>
+                <span>{0.002}</span>
+                <span> ETH</span>
+              </p>
+            </div>
+            <button className={styles.cardMint} onClick={() => mintHope()}>
+              Mint Hope
+            </button>
+          </div>
+          <div className={styles.card}>
+            <p className={styles.cardMinted}>{mintedFlashes}/3000</p>
+            <h1 className={styles.cardTitle}>Flash</h1>
+            <p className={styles.cardDesc}>
+              Bullet is a horse with insane speed. Speed Increment: 9.
+            </p>
+            <div className={styles.cardPriceCont}>
+              <p className={styles.cardPrice}>
+                <span>{0.003}</span>
+                <span> ETH</span>
+              </p>
+            </div>
+            <button className={styles.cardMint} onClick={() => mintFlash()}>
+              Mint Flash
+            </button>
+          </div>
+        </div>
       ) : (
-        <div className="container mx-auto">Please connect to a supported chain</div>
+        <div className="container mx-auto">
+          Please connect to a supported chain
+        </div>
       )}
-      
     </div>
-  )
-}
+  );
+};
 
-export default Mint
+export default Mint;
